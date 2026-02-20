5c7c662d49bb60217fed47f8fa53dc435d328019
Pablo Sanchez <pablogsa30@gmail.com>
2026-02-20 08:39:54 -0400

    first commit

---
+++ cursos.js
@@ -0,0 +1,43 @@
+let infoCursos = {
+    programacion: [
+        {
+            id: 1,
+            titulo: "aprende python",
+            lenguaje: "pyhton",
+            vistas: 1500,
+            nivel: "basico"
+        },
+        {
+            id: 2,
+            titulo: "aprende python",
+            lenguaje: "pyhton",
+            vistas: 1500,
+            nivel: "intermedio"
+        },
+        {
+            id: 3,
+            titulo: "aprende JavaScript2",
+            lenguaje: "javascript",
+            vistas: 102223,
+            nivel: "basico"
+        }
+    ],
+    matematicas: [
+        {
+            id: 1,
+            titulo: "aprende Calculo",
+            tema: "calculo",
+            vistas: 12427,
+            nivel: "basico"
+        },
+        {
+            id: 2,
+            titulo: "aprende Algebra",
+            tema: "algebra",
+            vistas: 2500,
+            nivel: "intermedio"
+        }
+    ]
+}
+
+module.exports.infoCursos = infoCursos;
